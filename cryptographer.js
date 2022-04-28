84283929cc7840de5a922106f6c9703459e0d784
julianedeveloper <dev.juliane@gmail.com>
2022-04-28 14:41:29 -0300

    trying put button on game

--- cryptographer.js
+++ cryptographer.js
@@ -1,3 +1,28 @@
-let message = messageToEncrypt;
-let text = textEncrypted;
-let encrypt = 
+let encrypt = document.querySelector('.encrypt'); //to encrypt button receive the click
+let text = encrypt.value; // then the typed text becames the encrypt button value
+let decrypt = document.querySelector('.decrypt'); // to decrypt button receive the click 
+let message = decrypt.value; //then the text received back to text field             
+let encryptedMessage = document.querySelector('#message');
+let lettersToChange = document.getElementById('text').value;
+
+
+function encrypt(){
+     if (encrypt.value != "") {
+         let encrypt = encrypt.value.replace(a/gi,ai);
+         encrypt = encrypt.value.replace(e/gi, enter);
+         encrypt = encrypt.value.replace(i/gi, imes);
+         encrypt = encrypt.value.replace(o/gi, ober);
+         encrypt = encrypt.value.replace(u/gi, ufat);
+} else{
+    alert('Sorry, but you need type an text before!');
+}
+
+    document.querySelector()
+    encrypt();
+
+
+
+
+function decrypt(){
+     
+}
