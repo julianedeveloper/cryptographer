3ea3f397329477e2c373a6f5065f647b53a3eea4
julianedeveloper <dev.juliane@gmail.com>
2022-04-28 17:21:14 -0300

    solving some questions

--- cryptographer.js
+++ cryptographer.js
@@ -1,28 +1,21 @@
 let encrypt = document.querySelector('.encrypt'); //to encrypt button receive the click
 let text = encrypt.value; // then the typed text becames the encrypt button value
 let decrypt = document.querySelector('.decrypt'); // to decrypt button receive the click 
-let message = decrypt.value; //then the text received back to text field             
-let encryptedMessage = document.querySelector('#message');
-let lettersToChange = document.getElementById('text').value;
+let message = decrypt.value; //then the text received back to text field    
+
+let encrypted = document.querySelector('#message');  // text typed by user first
+let toEncrypt = document.getElementById('text').value; // text encrypted already
 
 
 function encrypt(){
-     if (encrypt.value != "") {
-         let encrypt = encrypt.value.replace(a/gi,ai);
-         encrypt = encrypt.value.replace(e/gi, enter);
-         encrypt = encrypt.value.replace(i/gi, imes);
-         encrypt = encrypt.value.replace(o/gi, ober);
-         encrypt = encrypt.value.replace(u/gi, ufat);
-} else{
-    alert('Sorry, but you need type an text before!');
-}
+    for (variavel = 0; lettersToChange.length > 5; variavel ++) {
+        
+        
+         encrypt.value.replace(a/gi,ai);
+         encrypt.value.replace(e/gi, enter);
+         encrypt.value.replace(i/gi, imes);
+         encrypt.value.replace(o/gi, ober);
+         encrypt.value.replace(u/gi, ufat);
 
-    document.querySelector()
-    encrypt();
-
-
-
-
-function decrypt(){
-     
+    }
 }
